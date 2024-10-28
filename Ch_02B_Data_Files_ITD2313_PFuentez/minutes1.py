f04c52f0d95065f26fb1e6a948b3e40b3f8848d4
Pedro Fuentez <pedro.fuentez@okstate.edu>
2024-10-28 01:09:25 -0500

    Delete Ch_02B_Data_Files_ITD2313_PFuentez directory

--- Ch_02B_Data_Files_ITD2313_PFuentez/minutes1.py
+++
@@ -1,6 +0,0 @@
-years = int(input("Enter the number of years: "))
-
-minutes_per_year = 365 * 24 * 60
-total_minutes = years * minutes_per_year
-
-print("The number of minutes in", years, "years is:", total_minutes)
